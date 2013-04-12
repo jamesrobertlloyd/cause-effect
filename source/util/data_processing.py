1de9e7544892719fa81a56aa9bbb72b3cbfa71ed
James Robert Lloyd <jrl44@cam.ac.uk>
2013-04-12 15:20:39 +0100

    Added a function to count data set sizes

--- source/util/data_processing.py
+++ source/util/data_processing.py
@@ -3,6 +3,7 @@ Various utilities relating to basic pre-processing
 '''
 
 import numpy as np
+from pylab import *
 
 def create_random_predictors():
     '''Creates the example files in the predictors directory'''
@@ -22,4 +23,20 @@ def create_random_predictors():
         save_file.write('SampleID,A->B,B->A,A-B or A|B\n')
         random_lines = ['%s,%f,%f,%f' % ((ID,) + tuple(np.random.dirichlet([1] * 3))) for ID in IDs]
         save_file.write('\n'.join(random_lines))
+        
+def count_size_of_data_sets():
+    '''Draws a histogram of the data set sizes'''
+    # First load all data
+    all_lines = open('../data/raw/PUBLIC/CEdata_train_pairs.csv', 'r').readlines()[1:] + \
+                open('../data/raw/PUBLIC/CEdata_valid_pairs.csv', 'r').readlines()[1:]
+    # Now compute data set sizes
+    sizes = [len(line.split(',')[1].split(' ')) for line in all_lines]
+    print 'Min size = %d : Max size = %d' % (min(sizes), max(sizes))
+    # Plot a histogram
+    clf()
+    hist(sizes, bins=50)
+    xlabel('Number of data points')
+    ylabel('Count')
+    title('Histogram of data set sizes')
+    show()
 
