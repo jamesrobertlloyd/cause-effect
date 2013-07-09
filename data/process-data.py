c159fc031d63673fa6db5c1a8d0c218e287ee1b2
James Robert Lloyd <jrl44@cam.ac.uk>
2013-07-09 11:23:32 +0100

    Data processing less memory intensive

--- data/process-data.py
+++ data/process-data.py
@@ -1,8 +1,14 @@
 import numpy as np
+import os
+
 from counter import Progress
 
-def combine():
+def combine(overwrite=False):
     # Loads the various data sources and concatenates them
+    
+    if (not overwrite) and os.path.exists('training/CEdata_train_pairs.csv') :
+        print 'Output already exists - not overwriting'
+        return
 
     with open('raw/CEfinal_train_pairs.csv', 'r') as pairs_data_file:
         pairs_header = pairs_data_file.readline()
@@ -75,30 +81,11 @@ def combine():
     with open('training/CEdata_train_publicinfo.csv', 'w') as info_data_file:
         info_data_file.write(info_header + ''.join(info_body))
 
-def reverse_it():
-    # Open pairs
-    with open('training/CEdata_train_pairs.csv', 'r') as pairs_data_file:
-        pairs_header = pairs_data_file.readline()
-        pairs_body = pairs_data_file.readlines()
-    # Reverse it
-    original_length = len(pairs_body)
-    prog = Progress(original_length)
-    for i in range(original_length):
-        A = np.array([float(a) for a in pairs_body[i].strip().split(',')[1].strip().split(' ')])
-        B = np.array([float(b) for b in pairs_body[i].strip().split(',')[2].strip().split(' ')])
-        if set(A) == set([0, 1]):
-            A_reversed = 1 - A
-        else:
-            A_reversed = 2 * np.mean(A) - A
-        if set(B) == set([0, 1]):
-            B_reversed = 1 - B
-        else:
-            B_reversed = 2 * np.mean(B) - B
-        pairs_body.append(','.join(['train%d' % (len(pairs_body)+1)] + [' '.join(str(a) for a in A_reversed)] + [' '.join(str(b) for b in B)]) + '\n')
-        pairs_body.append(','.join(['train%d' % (len(pairs_body)+1)] + [' '.join(str(a) for a in A)] + [' '.join(str(b) for b in B_reversed)]) + '\n')
-        pairs_body.append(','.join(['train%d' % (len(pairs_body)+1)] + [' '.join(str(a) for a in A_reversed)] + [' '.join(str(b) for b in B_reversed)]) + '\n')
-        prog.tick()
-    prog.done()
+def reverse_it(overwrite=False):
+    
+    if (not overwrite) and os.path.exists('training-reversed/CEdata_train_pairs.csv') :
+        print 'Output already exists - not overwriting'
+        return
         
     # Open info
     with open('training/CEdata_train_publicinfo.csv', 'r') as info_data_file:
@@ -121,11 +108,44 @@ def reverse_it():
         target_body.append(','.join(['train%d' % (len(target_body)+1)] + target_body[i].split(',')[1:]))
         target_body.append(','.join(['train%d' % (len(target_body)+1)] + target_body[i].split(',')[1:]))
         target_body.append(','.join(['train%d' % (len(target_body)+1)] + target_body[i].split(',')[1:]))
-
-    # Save files
-
+        
+    # Open pairs
+    with open('training/CEdata_train_pairs.csv', 'r') as pairs_data_file:
+        pairs_header = pairs_data_file.readline()
+        # Write reversed lines to temporary file
+        with open('temp.csv', 'w') as temp_file:
+            temp_file.write(pairs_header)
+            prog = Progress(original_length * 5)
+            for line in pairs_data_file:
+                A = np.array([float(a) for a in line.strip().split(',')[1].strip().split(' ')])
+                B = np.array([float(b) for b in line.strip().split(',')[2].strip().split(' ')])
+                if set(A) == set([0, 1]):
+                    A_reversed = 1 - A
+                else:
+                    A_reversed = 2 * np.mean(A) - A
+                if set(B) == set([0, 1]):
+                    B_reversed = 1 - B
+                else:
+                    B_reversed = 2 * np.mean(B) - B
+                temp_file.write(','.join(['dummy-id'] + [' '.join(str(a) for a in A_reversed)] + [' '.join(str(b) for b in B)]) + '\n')
+                temp_file.write(','.join(['dummy-id'] + [' '.join(str(a) for a in A)] + [' '.join(str(b) for b in B_reversed)]) + '\n')
+                temp_file.write(','.join(['dummy-id'] + [' '.join(str(a) for a in A_reversed)] + [' '.join(str(b) for b in B_reversed)]) + '\n')
+                prog.tick()
+                
+    # Concatenate original pairs and temporary file 
     with open('training-reversed/CEdata_train_pairs.csv', 'w') as pairs_data_file:
-        pairs_data_file.write(pairs_header + ''.join(pairs_body))
+        pairs_data_file.write(pairs_header)
+        i = 1
+        for file_name in ['training/CEdata_train_pairs.csv', 'temp.csv']:
+            with open(file_name, 'r') as input_file:
+                input_file.readline()
+                for line in input_file:
+                    pairs_data_file.write(','.join(['train%d' % i] + line.split(',')[1:]))
+                    prog.tick()
+                    i += 1
+    prog.done()    
+    os.remove('temp.csv') 
+    # Save other files
         
     with open('training-reversed/CEdata_train_target.csv', 'w') as target_data_file:
         target_data_file.write(target_header + ''.join(target_body))
@@ -133,18 +153,11 @@ def reverse_it():
     with open('training-reversed/CEdata_train_publicinfo.csv', 'w') as info_data_file:
         info_data_file.write(info_header + ''.join(info_body))
         
-def flip_reverse_it():
-    # Open pairs
-    with open('training-reversed/CEdata_train_pairs.csv', 'r') as pairs_data_file:
-        pairs_header = pairs_data_file.readline()
-        pairs_body = pairs_data_file.readlines()
-    # Flip it
-    original_length = len(pairs_body)
-    prog = Progress(original_length)
-    for i in range(original_length):
-        pairs_body.append(','.join(['train%d' % (len(pairs_body)+1)] + list(reversed(pairs_body[i].strip().split(',')[1:]))) + '\n')
-        prog.tick()
-    prog.done()
+def flip_reverse_it(overwrite=False):
+    
+    if (not overwrite) and os.path.exists('training-flipped-reversed/CEdata_train_pairs.csv') :
+        print 'Output already exists - not overwriting'
+        return
         
     # Open info
     with open('training-reversed/CEdata_train_publicinfo.csv', 'r') as info_data_file:
@@ -178,11 +191,33 @@ def flip_reverse_it():
         target_body.append(','.join(['train%d' % (len(target_body)+1)] + targets))
         prog.tick()
     prog.done()
-
-    # Save files
-
+    
+    # Open pairs
+    with open('training-reversed/CEdata_train_pairs.csv', 'r') as pairs_data_file:
+        pairs_header = pairs_data_file.readline()
+        with open('temp.csv', 'w') as temp_file:
+            temp_file.write(pairs_header)
+            prog = Progress(original_length * 3)
+            # Save flipped lines to temporary file
+            for line in pairs_data_file:
+                temp_file.write(','.join(['dummy-id'] + list(reversed(line.strip().split(',')[1:]))) + '\n')
+                prog.tick()
+        
+    # Concatenate original pairs and temporary file 
     with open('training-flipped-reversed/CEdata_train_pairs.csv', 'w') as pairs_data_file:
-        pairs_data_file.write(pairs_header + ''.join(pairs_body))
+        pairs_data_file.write(pairs_header)
+        i = 1
+        for file_name in ['training-reversed/CEdata_train_pairs.csv', 'temp.csv']:
+            with open(file_name, 'r') as input_file:
+                input_file.readline()
+                for line in input_file:
+                    pairs_data_file.write(','.join(['train%d' % i] + line.split(',')[1:]))
+                    prog.tick()
+                    i += 1
+    prog.done()    
+    os.remove('temp.csv')
+
+    # Save other files
         
     with open('training-flipped-reversed/CEdata_train_target.csv', 'w') as target_data_file:
         target_data_file.write(target_header + ''.join(target_body))
@@ -190,18 +225,11 @@ def flip_reverse_it():
     with open('training-flipped-reversed/CEdata_train_publicinfo.csv', 'w') as info_data_file:
         info_data_file.write(info_header + ''.join(info_body))
         
-def flip_it():
-    # Open pairs
-    with open('training/CEdata_train_pairs.csv', 'r') as pairs_data_file:
-        pairs_header = pairs_data_file.readline()
-        pairs_body = pairs_data_file.readlines()
-    # Flip it
-    original_length = len(pairs_body)
-    prog = Progress(original_length)
-    for i in range(original_length):
-        pairs_body.append(','.join(['train%d' % (len(pairs_body)+1)] + list(reversed(pairs_body[i].strip().split(',')[1:]))) + '\n')
-        prog.tick()
-    prog.done()
+def flip_it(overwrite=True):
+    
+    if (not overwrite) and os.path.exists('training-flipped/CEdata_train_pairs.csv') :
+        print 'Output already exists - not overwriting'
+        return
         
     # Open info
     with open('training/CEdata_train_publicinfo.csv', 'r') as info_data_file:
@@ -235,11 +263,31 @@ def flip_it():
         target_body.append(','.join(['train%d' % (len(target_body)+1)] + targets))
         prog.tick()
     prog.done()
-
-    # Save files
-
+    
+    # Open pairs
+    with open('training/CEdata_train_pairs.csv', 'r') as pairs_data_file:
+        pairs_header = pairs_data_file.readline()
+        with open('temp.csv', 'w') as temp_file:
+            temp_file.write(pairs_header)
+            prog = Progress(original_length * 3)
+            # Save flipped lines to temporary file
+            for line in pairs_data_file:
+                temp_file.write(','.join(['dummy-id'] + list(reversed(line.strip().split(',')[1:]))) + '\n')
+                prog.tick()
+        
+    # Concatenate original pairs and temporary file 
     with open('training-flipped/CEdata_train_pairs.csv', 'w') as pairs_data_file:
-        pairs_data_file.write(pairs_header + ''.join(pairs_body))
+        pairs_data_file.write(pairs_header)
+        i = 1
+        for file_name in ['training/CEdata_train_pairs.csv', 'temp.csv']:
+            with open(file_name, 'r') as input_file:
+                input_file.readline()
+                for line in input_file:
+                    pairs_data_file.write(','.join(['train%d' % i] + line.split(',')[1:]))
+                    prog.tick()
+                    i += 1
+
+    # Save other files
         
     with open('training-flipped/CEdata_train_target.csv', 'w') as target_data_file:
         target_data_file.write(target_header + ''.join(target_body))
